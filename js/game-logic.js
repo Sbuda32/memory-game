4330f3af1d844d1e79c23e5fb1bd7f7d07da999a
sbuda32 <sbuda32@gmail.com>
2019-04-18 12:13:36 +0200

    modified 2 files index.html and game-logic.js, Added jquery-3.4.0.js uncompressed file

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,3 +1,30 @@
 $(function(){
-    $('#myCanvas')
+    var memoryArray = ['I','I','II','II','III','III','IV','IV','V','V','VI','VI','VII','VII','VIII','VIII'];
+    memory_values = [];
+    memory_tile_ids = [];
+    var tilesFlipped = 0;
+
+    Array.prototype.shuffleMemoryArray = function(){
+
+        var i = this.length, j, temp;
+
+        while(--i > 0){
+
+            j = Math.floor(Math.random() * (i + 1));
+            temp = this[j];
+            this[j] = this[i];
+            this[i] = temp;
+        }
+    }
+
+    function createNewBoard(){
+
+        tilesFlipped = 0;
+        var output = '';
+        console.log()
+        memoryArray.shuffleMemoryArray();
+        console.log(memoryArray);
+    }
+
+    createNewBoard();
 });
